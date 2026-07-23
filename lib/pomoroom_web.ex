4406c751b7352349955f00ec32144fd5d749c48a
evaRce <eva.arce123@hotmail.com>
2026-07-23 14:59:14 +0200

    feat: añade logo

--- lib/pomoroom_web.ex
+++ lib/pomoroom_web.ex
@@ -17,7 +17,7 @@ defmodule PomoroomWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images sounds favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images sounds favicon.ico pomoroom_logo.ico robots.txt)
 
   def router do
     quote do
