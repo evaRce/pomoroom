f2bc8fccc555aad52d718d86f814520cc6ddb88a
evaRce <eva.arce123@hotmail.com>
2026-07-24 21:53:59 +0200

    fix: usa Slot.Root en Button, no el namespace  completo

--- assets/components-shadcn/ui/button.tsx
+++ assets/components-shadcn/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { Slot } from "radix-ui"
+import { Slot as SlotPrimitive } from "radix-ui"
 
 import { cn } from "../../lib/utils"
 
@@ -42,7 +42,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
+    const Comp = asChild ? SlotPrimitive.Root : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
