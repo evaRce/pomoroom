474d7f7555a985ba3343cc6c592b332af342cb27
evaRce <eva.arce123@hotmail.com>
2026-08-24 13:49:30 +0200

    feat: limita los intentos de registro a 3 cada 10 minutos por IP

--- lib/pomoroom_web/live/sign_up.ex
+++ lib/pomoroom_web/live/sign_up.ex
@@ -2,8 +2,15 @@ defmodule PomoroomWeb.HomeLive.SignUp do
   use PomoroomWeb, :live_view
   alias Pomoroom.Users
 
+  @max_attempts 3
+  @scale_ms :timer.minutes(10)
+
   def mount(_params, session, socket) do
-    socket = assign(socket, :locale, Map.get(session, "locale", "es"))
+    socket =
+      socket
+      |> assign(:client_ip, client_ip(socket))
+      |> assign(:locale, Map.get(session, "locale", "es"))
+
     {:ok, PhoenixLiveSession.maybe_subscribe(socket, session), layout: false}
   end
 
@@ -13,7 +20,24 @@ defmodule PomoroomWeb.HomeLive.SignUp do
     {:noreply, assign(socket, :locale, locale)}
   end
 
-  def handle_event("action.save_user", params, socket) do
+  def handle_event(
+        "action.save_user",
+        params,
+        %{assigns: %{client_ip: client_ip}} = socket
+      ) do
+    case PomoroomWeb.RateLimiter.hit("sign_up:#{client_ip}", @scale_ms, @max_attempts) do
+      {:deny, _retry_after} ->
+        {:noreply,
+         push_event(socket, "react.error_save_user", %{
+           errors: %{email: gettext("Demasiados intentos. Inténtalo de nuevo en unos minutos")}
+         })}
+
+      {:allow, _count} ->
+        do_save_user(params, socket)
+    end
+  end
+
+  defp do_save_user(params, socket) do
     case Users.register_user(params) do
       {:ok, user_changes} ->
         user_info = Map.drop(user_changes, [:password])
@@ -22,6 +46,13 @@ defmodule PomoroomWeb.HomeLive.SignUp do
 
       {:error, reason} ->
         {:noreply, push_event(socket, "react.error_save_user", %{errors: reason})}
+    end
+  end
+
+  defp client_ip(socket) do
+    case get_connect_info(socket, :peer_data) do
+      %{address: address} -> address |> :inet.ntoa() |> to_string()
+      _ -> "unknown"
     end
   end
 end
