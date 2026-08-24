c099494bb4f496ee77a63de7af89741eda6c8065
evaRce <eva.arce123@hotmail.com>
2026-08-24 13:24:08 +0200

    fix: marca el fondo decorativo del chat como aria-hidden

--- assets/js/components/chat_room/chat_panel/BackGround.tsx
+++ assets/js/components/chat_room/chat_panel/BackGround.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import useChatPanelText from "./chatPanelText";
 
 export default function BackGround({ imageNumber }: { imageNumber: number }) {
-  const chatPanelText = useChatPanelText();
   return (
     <div className="flex flex-col flex-grow w-full border-l border-r justify-center items-center">
       <img
         src={`/images/background2/background-${imageNumber}.svg`}
-        alt={chatPanelText.background.alt}
+        alt=""
+        aria-hidden="true"
         className="object-cover w-full h-full opacity-70"
       />
     </div>
