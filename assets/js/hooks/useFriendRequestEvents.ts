2097060ddd17982aaf039f9f38d9cf6960c99fe6
evaRce <eva.arce123@hotmail.com>
2026-09-03 17:30:30 +0200

    fix: revierte limpieza de pantallas de rechazo al cancelar peticion

--- assets/js/hooks/useFriendRequestEvents.ts
+++ assets/js/hooks/useFriendRequestEvents.ts
@@ -114,12 +114,7 @@ export function useFriendRequestEvents({
       (userNickname === eventData.from_user || userNickname === eventData.to_user)
     ) {
       addEvent(eventName, { from_user: eventData.from_user, to_user: eventData.to_user });
-      if (
-        component === "RequestSend" ||
-        component === "RequestReceived" ||
-        component === "RejectedRequestSend" ||
-        component === "RejectedRequestReceived"
-      ) {
+      if (component === "RequestSend" || component === "RequestReceived") {
         setComponent("");
       }
     }
